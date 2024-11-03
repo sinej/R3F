897bfd569c26f9f97e28b5e847de42f6de3815fc
sinej <sinej1020@gmail.com>
2024-11-03 22:26:26 +0900

    feat: directionalLight 추가

--- src/threeElement.tsx
+++ src/threeElement.tsx
@@ -3,6 +3,7 @@ import * as THREE from "three";
 const ThreeElement = () => {
   return (
     <>
+      <directionalLight/>
       <directionalLight position={[5,5,5]} />
       <mesh rotation={[THREE.MathUtils.degToRad(45),THREE.MathUtils.degToRad(30),0]}>
         <boxGeometry />
