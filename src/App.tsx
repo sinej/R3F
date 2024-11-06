c2b8f83ff36ffb57a289a96eb1e11d3a4a7d086a
sinej <sinej1020@gmail.com>
2024-11-06 21:25:08 +0900

    feat: gridHelper 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Canvas } from "@react-three/fiber";
 import ThreeElement from "./threeElement";
 import { CameraControls } from "@react-three/drei";
-
+import * as THREE from "three";
 function App() {
 
   return (
@@ -10,7 +10,10 @@ function App() {
       <Canvas>
         <ThreeElement />
         <CameraControls />
-        <axesHelper />
+        <axesHelper args={[7]} />
+        <gridHelper args={[10, 10, 'red', 'blue']}
+                    rotation={[THREE.MathUtils.degToRad(90), 0, 0]}
+        />
       </Canvas>
     </>
   )
