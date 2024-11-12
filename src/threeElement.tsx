68fd048d1d217f8f80f391116626b27a2cf50082
sinej <sinej1020@gmail.com>
2024-11-12 09:25:34 +0900

    feat: normalized Vector 추가

--- src/threeElement.tsx
+++ src/threeElement.tsx
@@ -4,46 +4,79 @@ const ThreeElement = () => {
 
   const origin = new THREE.Vector3(0,0,0);
 
-  const vecA = new THREE.Vector3(-6,-7,0).multiplyScalar(2);
+  const vecA = new THREE.Vector3(-6,7,0);
   const disVecA = origin.distanceTo(vecA);
 
-  const vecB = new THREE.Vector3(4,3,0).multiplyScalar(2);
+  const vecB = new THREE.Vector3(4,3,0);
   const disVecB = origin.distanceTo(vecB);
 
-  const vecC = vecB.clone().sub(vecA);
+  const vecC = vecA.clone().add(vecB);
   const disVecC = origin.distanceTo(vecC);
 
+  const normalizedVecA = vecA.clone().normalize();
+  const disNormalizedVecA = origin.distanceTo(normalizedVecA);
 
   return (
     <>
-      <directionalLight position={[5,5,5]} />
+      <directionalLight position={[5, 5, 5]}/>
+      {/* VecA */}
       <mesh
-            position={vecA}
-            rotation={[THREE.MathUtils.degToRad(45), THREE.MathUtils.degToRad(45), 0]}
+        position={vecA}
+        rotation={[THREE.MathUtils.degToRad(45), THREE.MathUtils.degToRad(45), 0]}
       >
-        <sphereGeometry args={[0.5]} />
-        <meshStandardMaterial color="red" />
+        <sphereGeometry args={[0.5]}/>
+        <meshStandardMaterial color="black"/>
       </mesh>
       <arrowHelper args={[
         vecA.clone().normalize(),
         origin,
         disVecA,
-        'blue'
+        'black'
       ]}/>
 
-
+      {/* Vec B */}
       <mesh
-            position={vecB}
-            rotation={[THREE.MathUtils.degToRad(45), THREE.MathUtils.degToRad(45), 0]}
+        position={vecB}
+        rotation={[THREE.MathUtils.degToRad(45), THREE.MathUtils.degToRad(45), 0]}
       >
-        <sphereGeometry args={[0.5]} />
-        <meshStandardMaterial color="red" />
+        <sphereGeometry args={[0.5]}/>
+        <meshStandardMaterial color="red"/>
       </mesh>
       <arrowHelper args={[
         vecB.clone().normalize(),
         origin,
         disVecB,
-        'blue'
+        'red'
+      ]}/>
+
+      {/* Vec C */}
+      <mesh
+        position={vecC}
+        rotation={[THREE.MathUtils.degToRad(45), THREE.MathUtils.degToRad(45), 0]}
+      >
+        <sphereGeometry args={[0.5]}/>
+        <meshStandardMaterial color="black" opacity={0.5} transparent />
+      </mesh>
+      <arrowHelper args={[
+        vecC.clone().normalize(),
+        origin,
+        disVecC,
+        'black'
+      ]}/>
+
+      {/* normalizedVecA */}
+      <mesh
+        position={normalizedVecA}
+        rotation={[THREE.MathUtils.degToRad(45), THREE.MathUtils.degToRad(45), 0]}
+      >
+        <sphereGeometry args={[0.5]}/>
+        <meshStandardMaterial color="black"/>
+      </mesh>
+      <arrowHelper args={[
+        normalizedVecA.clone().normalize(),
+        origin,
+        disNormalizedVecA,
+        'red'
       ]}/>
     </>
   );
