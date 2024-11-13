1c5b877f869692f40809bc23dbdf3592cbbe8364
sinej <sinej1020@gmail.com>
2024-11-13 09:03:45 +0900

    feat: multiplyScalar 스칼라 추가 (곰돌이 이동)

--- src/BearToElement.tsx
+++ src/BearToElement.tsx
@@ -1,25 +1,42 @@
 import * as THREE from "three";
-import {useLoader} from "@react-three/fiber";
+import {useFrame, useLoader} from "@react-three/fiber";
+import {useRef} from "react";
 
 
 const BearToElement = () => {
 
+  const bearRef = useRef<THREE.Sprite>(null);
+
   const bearTexture = useLoader(THREE.TextureLoader, '/bear.png');
   const honeyTexture = useLoader(THREE.TextureLoader, '/honey.png');
-  
+
+  const bearVector = new THREE.Vector3(-6, 7, 0);
+  const honeyVector = new THREE.Vector3(4, 3, 0);
+
+  const bearToHoneyVector = honeyVector.clone().sub(bearVector);
+  const bearToHoneyUnitVector = bearToHoneyVector.clone().normalize();
+
+  const desireMoveDistance = 0.1;
+  const moveVector = bearToHoneyUnitVector.clone().multiplyScalar(desireMoveDistance);
+
+
+  useFrame(() => {
+    const bearObj = bearRef.current;
+
+    if(bearObj) {
+      bearObj.position.x += moveVector.x;
+      bearObj.position.y += moveVector.y;
+    }
+  });
+
+
   return (
     <>
-      <sprite position={[0,0,0]}>
-        <spriteMaterial
-          rotation={90}
-          map={bearTexture}
-        />
+      <sprite ref={bearRef} position={bearVector}>
+        <spriteMaterial map={bearTexture} />
       </sprite>
-      <sprite position={[0,0,0]}>
-        <spriteMaterial
-          rotation={90}
-          map={honeyTexture}
-        />
+      <sprite position={honeyVector}>
+        <spriteMaterial map={honeyTexture} />
       </sprite>
     </>
   );
