17bd9a794591051a3bff76f941283a6ff92a674f
sinej <sinej1020@gmail.com>
2024-12-09 09:09:59 +0900

    feat: ball 10개 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Canvas } from "@react-three/fiber";
 import { CameraControls } from "@react-three/drei";
 import * as THREE from "three";
-import MovingSphere from "./MovingSphere.tsx";
+import MovingSpheres from "./MovingSpheres.tsx";
 // import BearToElement from "./BearToElement.tsx";
 function App() {
 
@@ -16,7 +16,8 @@ function App() {
       >
         {/*<ThreeElement />*/}
         {/*<BearToElement/>*/}
-        <MovingSphere />
+        {/*<MovingSphere />*/}
+        <MovingSpheres />
         <CameraControls />
         <axesHelper args={[7]} />
         <gridHelper args={[100, 100]}
--- src/MovingSpheres.tsx
+++ src/MovingSpheres.tsx
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import {useFrame} from "@react-three/fiber";
 import {useRef} from "react";
 
 const MovingSpheres = () => {
@@ -19,86 +18,97 @@ const MovingSpheres = () => {
     posVectors.push(posVector);
   }
 
-  const targetX = THREE.MathUtils.randFloat(-posLimit, posLimit);
-  const targetY = THREE.MathUtils.randFloat(-posLimit, posLimit);
+  // const targetX = THREE.MathUtils.randFloat(-posLimit, posLimit);
+  // const targetY = THREE.MathUtils.randFloat(-posLimit, posLimit);
+  //
+  // const targetB = new THREE.Vector3(targetX, targetY, 0);
+  //
+  // const vecAToVecB = new THREE.Vector3();
+  // vecAToVecB.subVectors(targetB, vecA); // const vecAtoB2 = vecB.clone().sub(vecA); 와 같다.
+  // vecAToVecB.normalize();
 
-  const targetB = new THREE.Vector3(targetX, targetY, 0);
-
-  const vecAToVecB = new THREE.Vector3();
-  vecAToVecB.subVectors(targetB, vecA); // const vecAtoB2 = vecB.clone().sub(vecA); 와 같다.
-  vecAToVecB.normalize();
-
-
-  let velocity = 0.1;
-  const accelation = 0.0001;
-  const velocityLimit = 1;
-  const dirVector = vecAToVecB.clone();
+  // let velocity = 0.1;
+  // const accelation = 0.0001;
+  // const velocityLimit = 1;
+  // const dirVector = vecAToVecB.clone();
   // dirVector.multiplyScalar(velocity);
+  //
+  // const box = new THREE.Box3();
+  // const center = new THREE.Vector3();
+  // const size = new THREE.Vector3(10, 10, 0);
+  // box.setFromCenterAndSize(
+  //   center,
+  //   size,
+  // );
+  //
+  // const leftBox = center.x - size.x * 0.5;
+  // const rightBox = center.x + size.x * 0.5;
+  // const topBox = center.y + size.y * 0.5;
+  // const bottomBox = center.y - size.y * 0.5;
+  //
+  //
+  // useFrame(() => {
+  //
+  //   if(ballA.current) {
+  //     const posA = ballA.current.position;
+  //
+  //     if((posA.x - ballARadius) < leftBox || (posA.x + ballARadius) > rightBox) {
+  //       dirVector.x = -dirVector.x;
+  //     }
+  //
+  //     if((posA.y - ballARadius) < bottomBox || (posA.y + ballARadius) > topBox) {
+  //       dirVector.y = -dirVector.y;
+  //     }
+  //
+  //     velocity += accelation;
+  //
+  //     if (velocity >= velocityLimit) {
+  //       velocity = velocityLimit;
+  //     }
 
-  const box = new THREE.Box3();
-  const center = new THREE.Vector3();
-  const size = new THREE.Vector3(10, 10, 0);
-  box.setFromCenterAndSize(
-    center,
-    size,
-  );
+  //    const addPos = dirVector.clone().multiplyScalar(velocity)
+  //    ballA.current.position.add(addPos);
+  //
+  //    top, right, bottom, left 별 멈추는 기능
+  //    if(posA.x > leftBox && posA.x < rightBox && posA.y < topBox && posA.y > bottomBox) {
+  //      ballA.current.position.add(dirVector);
+  //    }
+  //
+  //    if(disBallAAndB > 0.1) {
+  //      ballA.current.position.add(dirVector);
+  //      ballA.current.position.add와 같은 작동을 한다.
+  //      ballA.current.position.x += dirVector.x;
+  //      ballA.current.position.y += dirVector.y;
+  //      ballA.current.position.z += dirVector.z;
+  //    }
+  //   }
+  // })
 
-  const leftBox = center.x - size.x * 0.5;
-  const rightBox = center.x + size.x * 0.5;
-  const topBox = center.y + size.y * 0.5;
-  const bottomBox = center.y - size.y * 0.5;
-
-
-  useFrame(() => {
-
-    if(ballA.current) {
-      const posA = ballA.current.position;
-
-      if((posA.x - ballARadius) < leftBox || (posA.x + ballARadius) > rightBox) {
-        dirVector.x = -dirVector.x;
-      }
-
-      if((posA.y - ballARadius) < bottomBox || (posA.y + ballARadius) > topBox) {
-        dirVector.y = -dirVector.y;
-      }
-
-      velocity += accelation;
-
-      if (velocity >= velocityLimit) {
-        velocity = velocityLimit;
-      }
-
-      const addPos = dirVector.clone().multiplyScalar(velocity)
-      // ballA.current.position.add(addPos);
-
-      // top, right, bottom, left 별 멈추는 기능
-      // if(posA.x > leftBox && posA.x < rightBox && posA.y < topBox && posA.y > bottomBox) {
-      //   ballA.current.position.add(dirVector);
-      // }
-
-      // if(disBallAAndB > 0.1) {
-      //  ballA.current.position.add(dirVector);
-      //  ballA.current.position.add와 같은 작동을 한다.
-      //  ballA.current.position.x += dirVector.x;
-      //  ballA.current.position.y += dirVector.y;
-      //  ballA.current.position.z += dirVector.z;
-      // }
-    }
-  })
+  console.log("posVectors", posVectors);
 
   return (
     <>
-      <mesh ref={ballA} position={vecA}>
-        <sphereGeometry args={[0.5]}/>
-        <meshBasicMaterial color="blue"/>
-      </mesh>
+      {/* 여러개 */}
+      {
+        posVectors.length > 0 ?
+          posVectors.map((posVector) =>
+            <>
+              <mesh ref={ballA} position={posVector}>
+                <sphereGeometry args={[ballARadius]} />
+                <meshBasicMaterial color="blue"/>
+              </mesh>
+            </>
+          )
+          : <></>
+      }
+
       {/* Target 공 */}
       {/*<mesh ref={ballB} position={vecB}>*/}
       {/*  <sphereGeometry args={[0.5]}/>*/}
       {/*  <meshBasicMaterial color="green"/>*/}
       {/*</mesh>*/}
 
-      <box3Helper args={[box, 'blue']} />
+      {/*<box3Helper args={[box, 'blue']} />*/}
     </>
   );
 };
