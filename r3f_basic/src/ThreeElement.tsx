9210c5f2e32d38d9e799aa5d17c7318402f9b984
sinej <sinej1020@gmail.com>
2025-06-26 22:14:23 +0900

    feat: useFrame rotate, scale 추가

--- r3f_basic/src/ThreeElement.tsx
+++ r3f_basic/src/ThreeElement.tsx
@@ -1,14 +1,29 @@
-import {useThree} from '@react-three/fiber';
 import * as THREE from 'three';
+import { useFrame, useThree } from '@react-three/fiber';
+import { useRef } from 'react';
 
 function ThreeElement() {
-  const { size } = useThree();
-  console.log("size", size);
+  const { size, gl, screen, camera} = useThree();
+  const boxRef = useRef<THREE.Mesh>(null);
+  console.log("gl", gl)
+
+  useFrame((state, delta, xrFrame) => {
+    console.log("boxRef.current", boxRef.current);
+    boxRef.current.rotation.x += delta;
+    boxRef.current.position.y -= 0.01;
+    boxRef.current.scale.z += 0.01;
+  })
 
   return (
     <>
       <directionalLight position={[5, 5, 5]}/>
-      <mesh rotation={[THREE.MathUtils.degToRad(0), THREE.MathUtils.degToRad(0), 0]}>
+      <mesh rotation={[
+        THREE.MathUtils.degToRad(45),
+        THREE.MathUtils.degToRad(45),
+        0
+      ]}
+            ref={boxRef}
+      >
         <boxGeometry/>
         <meshStandardMaterial color={'blue'}/>
       </mesh>
