baae33581f7276e28d5121287b79d6379f2153ef
sinej <sinej1020@gmail.com>
2025-06-30 09:35:48 +0900

    feat: lava useControl추가

--- r3f_basic/src/App.tsx
+++ r3f_basic/src/App.tsx
@@ -1,7 +1,13 @@
 import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 import ThreeElement from "./ThreeElement.tsx";
+import { useControls } from "leva";
 
 export default function App() {
+  const color = useControls({
+    value: 'green'
+  })
+
   return (
     <Canvas camera={{
               near: 1,
@@ -10,6 +16,10 @@ export default function App() {
               position: [5, 5, 0]
             }}
     >
+      <color attach='background' args={[color.value]} />
+      <OrbitControls minAzimuthAngle={-Math.PI / 4} maxAzimuthAngle={Math.PI / 4} />
+      <axesHelper args={[5]} />
+      <gridHelper args={[10, 10, 0]} />
       <ThreeElement />
     </Canvas>
   );
--- r3f_basic/src/ThreeElement.tsx
+++ r3f_basic/src/ThreeElement.tsx
@@ -1,18 +1,6 @@
 import * as THREE from 'three';
-import { useFrame, useThree } from '@react-three/fiber';
-import { useRef } from 'react';
 
 function ThreeElement() {
-  const { size, gl, screen, camera} = useThree();
-  const boxRef = useRef<THREE.Mesh>(null);
-  console.log("gl", gl)
-
-  useFrame((state, delta, xrFrame) => {
-    console.log("boxRef.current", boxRef.current);
-    boxRef.current.rotation.x += delta;
-    boxRef.current.position.y -= 0.01;
-    boxRef.current.scale.z += 0.01;
-  })
 
   return (
     <>
@@ -22,7 +10,6 @@ function ThreeElement() {
         THREE.MathUtils.degToRad(45),
         0
       ]}
-            ref={boxRef}
       >
         <boxGeometry/>
         <meshStandardMaterial color={'blue'} />
