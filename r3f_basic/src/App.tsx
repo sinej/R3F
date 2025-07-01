d3ba68fe9ef9d2fcd099b2d2a80a92ea1d74c3e3
sinej <sinej1020@gmail.com>
2025-07-01 23:33:33 +0900

    feat: home 컴포넌트 추가

--- r3f_basic/src/App.tsx
+++ r3f_basic/src/App.tsx
@@ -1,26 +1,8 @@
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import ThreeElement from "./ThreeElement.tsx";
-import { useControls } from "leva";
+import Home from "./components/home.tsx";
 
 export default function App() {
-  const color = useControls({
-    value: 'green'
-  })
 
   return (
-    <Canvas camera={{
-              near: 1,
-              far: 20,
-              fov: 75,
-              position: [5, 5, 0]
-            }}
-    >
-      <color attach='background' args={[color.value]} />
-      <OrbitControls minAzimuthAngle={-Math.PI / 4} maxAzimuthAngle={Math.PI / 4} />
-      <axesHelper args={[5]} />
-      <gridHelper args={[10, 10, 0]} />
-      <ThreeElement />
-    </Canvas>
+    <Home/>
   );
 }
